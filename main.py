2d963caee1dfefef604cb768daf43da01567f026
sharif <shahanur700@gmail.com>
2024-05-23 03:05:23 +0600

    first

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import requests
-import os
-from PIL import Image
-from IPython.display import IFrame
-
-url = 'https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-PY0101EN-SkillsNetwork/labs/Module%205/data/Example1.txt'
-path = os.path.join(os.getcwd(), 'example1.txt')
-r = requests.get(url)
-with open(path, 'wb') as f:
-    f.write(r.content)
+import pandas as pd
+URL = 'https://en.wikipedia.org/wiki/List_of_largest_banks'
+tables = pd.read_html(URL)
+df = tables[0]
+print(df)
