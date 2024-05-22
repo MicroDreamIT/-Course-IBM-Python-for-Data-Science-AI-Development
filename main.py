b1c16d31601a17bdf56d5045e33c3a256f2bad34
sharif <shahanur700@gmail.com>
2024-05-23 02:29:56 +0600

    first

--- main.py
+++ main.py
@@ -1,18 +1,4 @@
-import numpy as np
-import pandas as pd
-import warnings
-
-
-def warn(*args, **kwargs):
-    pass
-
-
-warnings.warn = warn
-warnings.filterwarnings('ignore')
-
-URL = "https://web.archive.org/web/20230902185326/https://en.wikipedia.org/wiki/List_of_countries_by_GDP_%28nominal%29"
-
-tables = pd.read_html(URL)
-df = tables[3]
-
-print(df)
+import requests
+import os
+from PIL import Image
+from IPython.display import IFrame
